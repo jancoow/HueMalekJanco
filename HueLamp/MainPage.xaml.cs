811a876e858f36272642f01281892da3906528e5
MalekSediqi <maleksediqi17@gmail.com>
2015-12-16 17:58:34 +0100

    kleine verandering

--- HueLamp/MainPage.xaml.cs
+++ HueLamp/MainPage.xaml.cs
@@ -26,107 +26,78 @@ namespace HueLamp
     public sealed partial class MainPage : Page
     {
         HueHandler hh;
-        Frame frame;
-        public MainPage(Frame frame)
+        public MainPage()
         {
             hh = new HueHandler();
-            this.frame = frame;
             this.InitializeComponent();
             DataContext = new MainViewModel(hh);
-            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
-
-            SystemNavigationManager.GetForCurrentView().BackRequested += (s, e) =>
-            {
-                if (frame.CanGoBack)
-                {
-                    frame.GoBack();
-                    e.Handled = true;
-                    SetOfLights.IsChecked = true;
-                }
-            };
-            this.MySplitview.Content = frame;
-
+            SystemNavigationManager.GetForCurrentView().BackRequested += OnBackRequested;
+            MyFrame.Navigate(typeof(Lights),hh);
+            PageTitle.Text = "Alle Hue Lampen";
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+               MyFrame.CanGoBack ?
+               AppViewBackButtonVisibility.Visible :
+               AppViewBackButtonVisibility.Collapsed;
         }
 
         private void MenuButton_Click(object sender, RoutedEventArgs e)
         {
             MySplitview.IsPaneOpen = !MySplitview.IsPaneOpen;
-            ((RadioButton)sender).IsChecked = false;
         }
 
-      
-        private void SetOfLights_Checked(object sender, RoutedEventArgs e)
+        private void OnBackRequested(object sender, BackRequestedEventArgs e)
         {
-            MySplitview.IsPaneOpen = false;
-
-            if (((RadioButton)sender).IsChecked == true)
+            if (Frame.CanGoBack)
             {
-                frame.Navigate(typeof(Lights), hh);
-            }
-            else
-            {
-                ((RadioButton)sender).IsChecked = false;
-            }
-             
-        }
-
-        private void SettingsButton_Checked(object sender, RoutedEventArgs e)
-        {
-            MySplitview.IsPaneOpen = false;
-
-            if (((RadioButton)sender).IsChecked == true)
-            {
-                frame.Navigate(typeof(Settings), hh);
-            }
-            else
-            {
-                ((RadioButton)sender).IsChecked = false;
+                e.Handled = true;
+                Frame.GoBack();
             }
         }
 
-        private void AboutButton_Checked(object sender, RoutedEventArgs e)
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            MySplitview.IsPaneOpen = false;
-
-            if (((RadioButton)sender).IsChecked == true)
+            if (ItemLights.IsSelected)
             {
-                frame.Navigate(typeof(AboutPage));
+                MyFrame.Navigate(typeof(Lights), hh);
+                PageTitle.Text = "Alle Hue Lampen";
             }
-            else
+            else if (ItemGroups.IsSelected)
             {
-                ((RadioButton)sender).IsChecked = false;
+                MyFrame.Navigate(typeof(Groups), hh);
+                PageTitle.Text = "Hue groepen";
             }
-        }
-
-        private void GroupsButton_Checked(object sender, RoutedEventArgs e)
-        {
-            MySplitview.IsPaneOpen = false;
-
-            if (((RadioButton)sender).IsChecked == true)
+            else if (ItemSettings.IsSelected)
             {
-                frame.Navigate(typeof(Groups), hh);
+                MyFrame.Navigate(typeof(Settings), hh);
+                PageTitle.Text = "Settings";
             }
-            else
+            else if (ItemAbout.IsSelected)
             {
-                ((RadioButton)sender).IsChecked = false;
+                MyFrame.Navigate(typeof(AboutPage), hh);
+                PageTitle.Text = "About";
             }
 
-        }
-
-        private void MySplitviewPane_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
-        {
-            if (e.Cumulative.Translation.X > 50)
-            {
-                MySplitview.IsPaneOpen = true;
-            }
         }
 
         private void Grid_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
         {
-            if (e.Cumulative.Translation.X < -50)
+            if (e.Cumulative.Translation.X > 20)
+            {
+                 MySplitview.IsPaneOpen = true;
+            }
+        }
+
+        private void MySplitviewPane_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (e.Cumulative.Translation.X < -20)
             {
                 MySplitview.IsPaneOpen = false;
             }
+        }
+
+        private void ListBox_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MySplitview.IsPaneOpen = false;
         }
     }
 }
